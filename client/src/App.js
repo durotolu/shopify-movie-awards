81100286dae8e685220d8f4bd2965284cf54a973
Modurotolu Olokode <durolawk@gmail.com>
2021-01-15 15:39:10 +0100

    added max nominees and storage fornominees

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Route, Link } from 'react-router-dom';
 import MovieList from './Movies/MovieList';
 import Movie from './Movies/Movie';
 import Nominees from './Movies/Nominees';
 
 function App() {
-  const [nomineesList, setNomineesList] = useState([]);
+  const savedNominees = localStorage.getItem('nominees');
+  const [nomineesList, setNomineesList] = useState(JSON.parse(savedNominees) || []);
   const [nomineesID, setNomineesID] = useState([]);
+
+  useEffect(() => {
+    localStorage.setItem('nominees', JSON.stringify(nomineesList))
+  }, [nomineesList])
 
   const addToNomineesList = movie => {
     if (nomineesList.length < 5) {
-        setNomineesList([...nomineesList, movie])
-        setNomineesID([...nomineesID, movie.imdbID])
+      setNomineesList([...nomineesList, movie])
+      setNomineesID([...nomineesID, movie.imdbID])
     }
   };
 
@@ -28,10 +33,12 @@ function App() {
   }
 
   return (
-    <div className='app'>
-      <Nominees list={nomineesList} removeFromNomineesList={removeFromNomineesList} />
-      <Route exact path="/" render={props => <MovieList {...props} nomineesID={nomineesID} addToNomineesList={addToNomineesList} />} />
-      <Route path='/:movie' component={Movie} />
+    <div>
+      <div className='App'>
+        <Nominees list={nomineesList} removeFromNomineesList={removeFromNomineesList} />
+        <Route exact path="/" render={props => <MovieList {...props} nomineesID={nomineesID} addToNomineesList={addToNomineesList} />} />
+        <Route path='/:movie' component={Movie} />
+      </div>
     </div>
   );
 }
