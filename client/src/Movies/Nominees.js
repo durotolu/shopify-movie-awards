8b49e08939aa8d1ad957d255e4fdfe70c6a48f2c
Modurotolu Olokode <durolawk@gmail.com>
2021-01-15 01:08:03 +0100

    added nominee component

---
+++ client/src/Movies/Nominees.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
+import './Nominees.css';
+
+const Nominees = ({ list, removeFromNomineesList }) => {
+
+  return (
+    <div className="nav">
+      <NavLink to="/"><div className="home-button">Home</div></NavLink>
+      <div className="saved-list">
+        <h3>Nominees:</h3>
+        {list.map(movie => {
+          return (
+            <div key={movie.imdbID}>
+              <NavLink to={`/${movie.imdbID}`}>
+                <div className="saved-movie">{movie.title}</div>
+                <div className="saved-movie"><em>{movie.year}</em></div>
+                {/* <div className="saved-movie">{movie.Released}</div> */}
+              </NavLink>
+              <button onClick={() => removeFromNomineesList(movie)}>Remove</button>
+            </div>
+          )
+        })}
+      </div>
+    </div>
+  )
+};
+
+export default Nominees;
